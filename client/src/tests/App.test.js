416f800f5404ba5938c38e48d3eba926ff07bc04
Jenine Steinberg <jenine.steinberg@gmail.com>
2020-06-24 23:42:39 -0700

    added in the axios mock test

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { mount, shallow, render } from 'enzyme';
+import axios from 'axios';
+
 
 import App from '../components/App.jsx';
 import Carousel from '../components/Carousel.jsx'
@@ -7,7 +9,7 @@ import MainPhoto from '../components/MainPhoto.jsx';
 import BackArrow from '../components/BackArrow.jsx';
 import ForwardArrow from '../components/ForwardArrow.jsx';
 
-describe('the App component!', () => {
+describe('the App component tests now commencing!', () => {
   it('Should render the MainPhoto, the Carousel, and both arrow components', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.containsMatchingElement(<MainPhoto/>)).toBe(true);
@@ -15,4 +17,46 @@ describe('the App component!', () => {
     expect(wrapper.containsMatchingElement(<BackArrow/>)).toBe(true);
     expect(wrapper.containsMatchingElement(<ForwardArrow/>)).toBe(true);
   });
-})
+
+  jest.mock('axios', () => {
+    const item = [
+      {
+        "lowRes": [
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/LowRes/Eyes/Eyeshadow1.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/LowRes/Eyes/Eyeshadow2.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/LowRes/Eyes/Eyeshadow3.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/LowRes/Eyes/Eyeshadow4.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/LowRes/Eyes/Eyeshadow5.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/LowRes/Eyes/Eyeshadow6.jpg"
+        ],
+        "highRes": [
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/HighRes/Eyes/Eyeshadow1.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/HighRes/Eyes/Eyeshadow2.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/HighRes/Eyes/Eyeshadow3.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/HighRes/Eyes/Eyeshadow4.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/HighRes/Eyes/Eyeshadow5.jpg",
+            "https://sephoraitems.s3-us-west-1.amazonaws.com/HighRes/Eyes/Eyeshadow6.jpg"
+        ],
+        "_id": 312,
+        "itemName": "Fantastic Concrete Shirt",
+        "type": "eyeshadow"
+      }
+    ]
+
+    return {
+      get: jest.fn(() => Promise.resolve(item))
+    };
+  })
+
+  test('Are we doing the thing when the component mounts?', () => {
+    const wrapper = shallow(<App />);
+    const mock = jest.fn();
+    wrapper.instance().getItemPhotos = mock;
+    wrapper.instance().forceUpdate();
+    wrapper
+      .instance()
+      .componentDidMount();
+    expect(mock).toHaveBeenCalled();
+  });
+
+});
